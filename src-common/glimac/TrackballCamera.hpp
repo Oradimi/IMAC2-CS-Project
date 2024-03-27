bb9cb7e6b19926c0fc609fd7171426599edccbed
Oradimi <loganargouse@gmail.com>
2024-03-27 19:26:19 +0100

    Small refactor to TrackballCamera
    Avoids separate values for connected purposes

--- src-common/glimac/TrackballCamera.hpp
+++ src-common/glimac/TrackballCamera.hpp
@@ -1,22 +1,24 @@
 #pragma once
 
 #include "glimac/math.hpp"
-#include "glm/ext/quaternion_exponential.hpp"
 #include "glm/gtx/transform.hpp"
 #include <algorithm>
 #include <glm/glm.hpp>
-#include <iostream>
+
+struct ViewAngle {
+  float x;
+  float y;
+};
 
 class TrackballCamera {
 private:
   float m_Distance;
-  float m_AngleX;
-  float m_AngleY;
+  ViewAngle m_Angle;
 
 public:
-  TrackballCamera() : m_Distance(173.2f), m_AngleX(0.f), m_AngleY(0.f){};
-  TrackballCamera(const float distance, const float angleX, const float angleY)
-      : m_Distance(distance), m_AngleX(angleX), m_AngleY(angleY){};
+  TrackballCamera() : m_Distance(173.2f), m_Angle(0.f){};
+  TrackballCamera(const float distance, const ViewAngle angle)
+      : m_Distance(distance), m_Angle(angle){};
 
   void moveFront(const float &delta) {
     constexpr float min_val = 0.4f;
@@ -27,12 +29,12 @@ public:
   }
 
   void rotateLeft(const float &degrees) {
-    m_AngleY += degrees;
+    m_Angle.y += degrees;
     // std::cout << m_AngleY << " " << m_AngleX << std::endl;
   }
 
   void rotateUp(float degrees) {
-    m_AngleX += degrees;
+    m_Angle.x += degrees;
     // std::cout << m_AngleY << " " << m_AngleX << std::endl;
   }
 
@@ -40,19 +42,11 @@ public:
     glm::mat4 ViewMatrix = glm::mat4(1.f);
     ViewMatrix = glm::translate(ViewMatrix, {0.f, 0.f, -m_Distance});
     ViewMatrix =
-        glm::rotate(ViewMatrix, glm::radians(m_AngleX), {1.f, 0.f, 0.f});
+        glm::rotate(ViewMatrix, glm::radians(m_Angle.x), {1.f, 0.f, 0.f});
     ViewMatrix =
-        glm::rotate(ViewMatrix, glm::radians(m_AngleY), {0.f, 1.f, 0.f});
+        glm::rotate(ViewMatrix, glm::radians(m_Angle.y), {0.f, 1.f, 0.f});
     return ViewMatrix;
   }
 
-  float getDistance() { return m_Distance; }
-
-  // glm::mat4 getViewMatrixInverse() const
-  // {
-  //     glm::mat4 MVMatrix = glm::translate(glm::mat4{1.f}, {0.f, 0.f,
-  //     -m_Distance}); return MVMatrix * glm::rotate(MVMatrix,
-  //     glm::radians(m_AngleX), {1.f, 0.f, 0.f}) * glm::rotate(MVMatrix,
-  //     glm::radians(m_AngleY), {0.f, 1.f, 0.f});
-  // }
+  float getDistance() const { return m_Distance; }
 };
