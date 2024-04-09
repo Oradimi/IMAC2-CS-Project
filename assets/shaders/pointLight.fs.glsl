7fc88d5eee6bc1bd8df200b7832c1769fbe76b91
Oradimi <loganargouse@gmail.com>
2024-04-09 14:18:19 +0200

    Improve light shaders

--- assets/shaders/pointLight.fs.glsl
+++ assets/shaders/pointLight.fs.glsl
@@ -15,11 +15,22 @@ in vec3 vNormal_vs;
 out vec4 fFragColor;
 
 vec3 blinnPhong() {
-    float d = distance(uLightPos_vs, vPosition_vs);
-    return (uLightIntensity / (d * d)) 
-    * (uKd * (dot(normalize(uLightPos_vs - vPosition_vs), normalize(vNormal_vs))) + uKs
-    * pow(dot(normalize((normalize(-vPosition_vs)
-    + normalize(uLightPos_vs - vPosition_vs)) / 2.f), normalize(vNormal_vs)), uShininess));
+    float distanceLightFragment = distance(uLightPos_vs, vPosition_vs);
+    vec3 nLightPosToPosition_vs = normalize(uLightPos_vs - vPosition_vs);
+    vec3 nNormal_vs = normalize(vNormal_vs);
+    vec3 nPosition_vs = normalize(-vPosition_vs);
+    
+    vec3 halfwayVector = normalize(nLightPosToPosition_vs + nPosition_vs);
+
+    float diffuseTerm = max(dot(nLightPosToPosition_vs, nNormal_vs), 0.0);
+    vec3 diffuseColor = uKd * diffuseTerm;
+
+    float specularTerm = pow(max(dot(nNormal_vs, halfwayVector), 0.0), uShininess);
+    vec3 specularColor = uKs * specularTerm;
+
+    float attenuation = 1.0 / (distanceLightFragment * distanceLightFragment);
+    
+    return uLightIntensity * attenuation * (diffuseColor + specularColor);
 }
 
 void main()
